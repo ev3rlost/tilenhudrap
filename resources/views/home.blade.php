9e6af694dca7373f083faf232fb74427dd1b67ee
ev3rlost <lenart.mlinar@gmail.com>
2022-08-03 23:54:36 +0200

    Added Wacken info and YT videos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
                         Welcome!
                     </h1>
                     <div class="paragraph text-center">
-                        Welcome to the official website of <span class="text-neutral-300 font-semibold">Tilen Hudrap</span> - a bassist, songwriter, producer, multi-instrumentalist and backing vocalist! Here you will find some basic info about Tilen's music path, the bands he performed with during the last 20+ years and more! Many more sections to come in the near future! Watch this space for updates!
+                        Welcome to the online realm of Tilen Hudrap, visitors from all dimensions! Here you'll find some basic info about Tilen's music path, the bands he performed with during the last 20+ years and more! Many sections will be added in the near future (past, present and future tour dates, multimedia, discography, press, gear talk, shop, testimonials, Hudy's Heavy Metal Academy and many, many more!)! Share the website with all interested parties and watch this space for future updates!
                     </div>
                     <div class="flex gap-8 mt-2">
                         <a href="{{ route('biography') }}" class="text-left md:text-center text-xs md:text-base shadow-md bg-none border-2 border-neutral-500 text-white font-semibold uppercase tracking-widest p-2 md:p-4 rounded hover:bg-brand/20 hover:ring-2 hover:ring-brand hover:border-transparent transition">learn more ...</a>
@@ -32,11 +32,32 @@
 
         </div>
 
+        <!-- Wacken Open Air 2022 -->
+        <div class="mb-8">
+            <div class="bg-black/40 p-4 md:p-8 rounded-xl flex gap-8">
+                <div class="w-1/3 space-y-8">
+                    <img src="{{ asset('/images/woa22.jpg') }}" alt="Wacken Open Air 2022">
+                </div>
+                <div class="w-2/3 text-neutral-400">
+                    <div class="text-brand mb-8">03/08/2022</div>
+                    Metalheads, tune in tomorrow, 4th of August 2022, between 18:30 and 20:00 PM Central European Time and watch the <a href="https://live.wacken.com" class="font-semibold text-white">LIVE STREAM</a> of the sold out Wacken Open Air where Dirkschneider will perform infront of 80 000 fans from fifty plus countries!<br><br>
+                    You can watch the livestream <a href="https://live.wacken.com" class="font-semibold text-white">HERE</a>!
+                </div>
+            </div>
+        </div>
+
         <!-- Videos -->
         <div class="flex flex-col gap-8">
+            <!-- U.D.O -->
             <iframe src="https://www.youtube.com/embed/sv29zn_gXY8" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-full aspect-video bg-black/40 p-4 md:p-8 rounded-xl"></iframe>
+            <!-- Dirkschneider -->
             <iframe src="https://www.youtube.com/embed/sWfSQDElZDg" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-full aspect-video bg-black/40 p-4 md:p-8 rounded-xl"></iframe>
+            <!-- Thraw -->
+            <iframe src="https://www.youtube.com/embed/e6ZsVzpnn-g" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-full aspect-video bg-black/40 p-4 md:p-8 rounded-xl"></iframe>
+            <!-- Pestilence -->
             <iframe src="https://www.youtube.com/embed/brrr5WT8vSA" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-full aspect-video bg-black/40 p-4 md:p-8 rounded-xl"></iframe>
+            <!-- Vicious Rumors -->
+            <iframe src="https://www.youtube.com/embed/F5updnhm9a4" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-full aspect-video bg-black/40 p-4 md:p-8 rounded-xl"></iframe>
         </div>
 
     </div>
